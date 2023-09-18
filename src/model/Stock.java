c4d9e52fb4e1b8fb190792129ec7091f19d26d03
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2023-09-18 12:35:37 -0300

    ♻️ refactor: padronizando idioma em Stock

--- src/model/Stock.java
+++ src/model/Stock.java
@@ -1,34 +1,38 @@
 package model;
 
 import dao.DAO;
-
 import java.util.List;
 
 public class Stock {
-    private int numLivros;
-    public Stock(int numLivros) {
-        this.numLivros = numLivros;
-    }
-    public Book addLivro(Book newbook){
-        return DAO.getBookDAO().creat(newbook);
-    }
-    public void apagarLivro(Book book){
-        DAO.getBookDAO().update(book);
-    }
-    /*
-    public void apagarTodos(){
-        DAO.getBookDAO().apagarTodos();
-    */
-    public Book atualizarLivro(Book book){
-        return DAO.getBookDAO().update(book);
-    }
-    /*
-    public Book getLivro(int isbn){
-        return DAO.getBookDAO().findByISBN(isbn);
+    private int numBooks;
+
+    public Stock(int numBooks) {
+        this.numBooks = numBooks;
     }
 
-    public List<Book> getLivros(){
-        return DAO.getBookDAO().encontrarTodos();
+    public Book addBook(Book newBook){
+        return DAO.getBookDAO().creat(newBook);
     }
-     */
+
+    public void deleteBook(Book book){
+        DAO.getBookDAO().delete(book);
+    }
+
+    /*
+    public void deleteAll(){ // não implementado
+        DAO.getBookDAO().apagarTodos();
+    */
+
+    public Book updateBook(Book book){
+        return DAO.getBookDAO().update(book);
+    }
+
+    public Book getBook(int isbn){
+        return DAO.getBookDAO().findById(isbn);
+    }
+
+    public List<Book> getBooks(){
+        return DAO.getBookDAO().findAll();
+    }
+
 }
