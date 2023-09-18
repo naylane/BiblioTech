e8bddddb9afa47b5d0453abe481b51bd98f06fb5
sarinhasf <sara2004.souza@gmail.com>
2023-09-17 22:10:24 -0300

    ✨feat: Classe Emprestimo

---
+++ src/model/Loan.java
@@ -0,0 +1,12 @@
+package model;
+import java.util.Date;
+public class Loan {
+    public int idLoan;
+    public int idUser;
+    public Book book;
+    public Date dateLoan;
+    public Date dateDevolution;
+    public int renovationQuantity;
+    public Boolean active;
+
+}
