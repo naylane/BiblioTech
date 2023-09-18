45bc188a398c386c873da447969d65ef67c53e9c
sarinhasf <sara2004.souza@gmail.com>
2023-09-17 22:02:38 -0300

    ✨feat: Classe Bibliotecario

---
+++ src/model/Librarian.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Librarian {
+    public void register_loan(Reader reader, Book book){
+    }
+    public void register_book(Book book){
+    }
+    public void register_devolution(Reader reader, Book book){
+    }
+}
