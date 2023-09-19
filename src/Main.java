7b375d4d1a0aff265a2608fbcc36e14d5152156f
sarinhasf <sara2004.souza@gmail.com>
2023-09-18 22:12:49 -0300

    🚧classe Main para teste

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         //criando livro e adicionando as informações na classe
         Book book = new Book(123456, "Título do Livro", "Autor do Livro",  //o bookdao é uma classe que ira mexer com o banco de dados
                 "Editora XYZ", 2023, "Ficção",
-                new BookLocation("1", "2", "XXX"), 3);
+                new BookLocation("1", "2", "XXX"), 3, reservationQueue);
 
         //Usando o DAO para adicionar o livro ao banco de dados
         BookDAO bookDao = DAO.getBookDAO();
