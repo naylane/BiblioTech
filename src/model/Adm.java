68a165d2660b6f0aad675e4bcc3ae6a2c174d7ef
sarinhasf <sara2004.souza@gmail.com>
2023-09-18 22:03:49 -0300

    🚧Classe Adm

--- src/model/Adm.java
+++ src/model/Adm.java
@@ -1,5 +1,11 @@
 package model;
 
-public class Adm extends Librarian {
+import java.util.Date;
 
+public class Adm extends Librarian {
+    public Adm(String name, String pin, String s, int age, int phone, Date registration_date, Residence address, Boolean block) {
+        super(name, pin, s, age, phone, registration_date, address, block);
+
+        //tem q criar a classe para criar usuarios
+    }
 }
