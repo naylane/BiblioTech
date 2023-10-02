ad45fb437f38bd6aa324f4bec95699cee572474b
sarinhasf <sara2004.souza@gmail.com>
2023-10-01 21:06:01 -0300

    🧪test: LibrarianDaoTestTest

--- src/test/dao/LibrarianDAOTest.java
+++
@@ -1,4 +0,0 @@
-package test.dao;
-
-public class LibrarianDAOTest {
-}
---
+++ src/test/dao/LibrarianDaoTest.java
@@ -0,0 +1,88 @@
+package test.dao;
+
+import dao.librarian.LibrarianDAOImpl;
+import model.Librarian;
+import model.Residence;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LibrarianDaoTest {
+    private LibrarianDAOImpl librarianDAO = new LibrarianDAOImpl();
+    private Librarian lib1;
+    private Librarian lib2;
+    private Librarian lib3;
+    Residence address;
+
+    @BeforeEach
+    public void setUp(){
+        address = new Residence("Estado", "Cidade", "Bairro", "Rua", 62, "40000000");
+        lib1 = new Librarian("Nome do Bibliotecario 1", "senha123","xx xxxxx-xxxx", address);
+        lib2 = new Librarian("Nome do Bibliotecario 2", "senha456","xx xxxxx-xxxx", address);
+        lib3 = new Librarian("Nome do Bibliotecario 3", "senha789","xx xxxxx-xxxx", address);}
+
+    @AfterEach
+    public void cleanDAO() {
+        librarianDAO.deleteAll();
+    }
+
+    @Test
+    public void testAddLibrarian() {
+        librarianDAO.creat(lib1);
+        Librarian libExpected = librarianDAO.findById(lib1.getId());
+        assertEquals(lib1, libExpected); // verifica se os objetos são iguais
+    }
+
+    @Test
+    public void testFindLibrarian() {
+        librarianDAO.creat(lib1);
+        assertSame(lib1, librarianDAO.findById(0));
+    }
+
+    @Test
+    public void testFindAll() {
+        librarianDAO.creat(lib1); // Adicionando bibliotecarios
+        librarianDAO.creat(lib2);
+        librarianDAO.creat(lib3);
+
+        assertEquals(3, librarianDAO.findAll().size()); //se add 3 elementos, logo tem que ter 3 elementos na lista
+    }
+
+    @Test
+    public void testUpdate() {
+        // Adicionando um adm
+        librarianDAO.creat(lib1);
+        // Criando bibliotecario com informações diferentes ao lib1
+        Librarian editedLibrarian = new Librarian("Nome Alterado", "Senha alterada", "xx xxxxx-xxxx", address);
+        // Editando
+        librarianDAO.update(editedLibrarian);
+
+        assertEquals(lib1, librarianDAO.findById(lib1.getId())); //verifica se realmente as alerações foram feitas
+    }
+
+    @Test
+    public void testDelete() {
+        // Adicionando dois bibliotecarios na lista
+        librarianDAO.creat(lib1);
+        librarianDAO.creat(lib2);
+        int qntBefore = librarianDAO.findAll().size();
+        // Removendo um bibliotecario
+        librarianDAO.delete(lib1);
+        //pegando a quantidade de elementos na lista
+        int qntAfter = librarianDAO.findAll().size();
+
+        assertTrue(qntAfter < qntBefore);
+    }
+
+    @Test
+    public void testDeleteAll() {
+        librarianDAO.deleteAll();
+        //verificando se a lista está vazia
+        assertTrue(librarianDAO.findAll().isEmpty());
+    }
+
+}
