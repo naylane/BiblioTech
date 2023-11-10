a02b4278d762efc2a745fa22700b68a4baacaaf8
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2023-11-10 06:30:46 -0300

    🚧Main provisória

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import dao.DAO;
+import model.*;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        /* ESCREVENDO NO ARQUIVO
+        Residence address = new Residence("state", "city", "neighbourhood", "street", 0, "cep");
+        Residence address1 = new Residence("state1", "city", "neighbourhood", "street", 44, "cep1");
+
+        Adm adm = new Adm("name adm", "pin", "phone", address);
+
+        adm.creatReader("Kenneth Dickson", "pin321", "xx xxxxx-xxxx", address);
+        adm.creatReader("Kevin Holloway", "123pin", "xx xxxxx-xxxx", address1);
+
+        // LENDO O ARQUIVO
+
+        System.out.println("ANTES");
+        Map<Long, Reader> readerMap = DAO.getReaderDAO().getReaderMap();
+        for (Map.Entry<Long, Reader> entry : readerMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        System.out.println("\nDEPOIS");
+        DAO.getReaderDAO().recoveryData();
+
+        Map<Long, Reader> readerMap1 = DAO.getReaderDAO().getReaderMap();
+
+        for (Map.Entry<Long, Reader> entry : readerMap1.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    */
+    }
+}
