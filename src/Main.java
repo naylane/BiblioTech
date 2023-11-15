aef9cbce97e91bb632cdcd059e42c83ee6ac5d40
sarinhasf <sara2004.souza@gmail.com>
2023-11-15 17:11:27 -0300

    🚧Main provisória

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,24 @@
 import dao.DAO;
 import model.*;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        /* ESCREVENDO NO ARQUIVO
-        Residence address = new Residence("state", "city", "neighbourhood", "street", 0, "cep");
-        Residence address1 = new Residence("state1", "city", "neighbourhood", "street", 44, "cep1");
+    public static void main(String[] args) throws IOException {
+        // ESCREVENDO NO ARQUIVO
+        //Residence address = new Residence("state", "city", "neighbourhood", "street", 0, "cep");
+        //Residence address1 = new Residence("state1", "city", "neighbourhood", "street", 44, "cep1");
+        //Adm adm = new Adm("name adm", "pin", "phone", address);
+        //adm.creatReader("Sara Maria ", "2563", "xx xxxxx-xxxx", address1);
 
-        Adm adm = new Adm("name adm", "pin", "phone", address);
+        // LENDO O ARQUIVO E COLOCANDO NO MAP
+        System.out.println("\nLENDO E COLOCANDO NO MAP");
+        DAO.getReaderDAO().recoverData();
 
-        adm.creatReader("Kenneth Dickson", "pin321", "xx xxxxx-xxxx", address);
-        adm.creatReader("Kevin Holloway", "123pin", "xx xxxxx-xxxx", address1);
-
-        // LENDO O ARQUIVO
-
-        System.out.println("ANTES");
+        System.out.println("LENDO O MAP");
         Map<Long, Reader> readerMap = DAO.getReaderDAO().getReaderMap();
         for (Map.Entry<Long, Reader> entry : readerMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-
-        System.out.println("\nDEPOIS");
-        DAO.getReaderDAO().recoveryData();
-
-        Map<Long, Reader> readerMap1 = DAO.getReaderDAO().getReaderMap();
-
-        for (Map.Entry<Long, Reader> entry : readerMap1.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-    */
-    }
-}
+}}
