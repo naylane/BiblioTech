49c1d53b01db195615a5f3bbc483a324ccd11e40
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2023-12-04 22:38:34 -0300

    📚 docs: javadoc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dao"},{"l":"dao.adm"},{"l":"dao.book"},{"l":"dao.librarian"},{"l":"dao.loan"},{"l":"dao.reader"},{"l":"dao.report"},{"l":"exceptions"},{"l":"model"},{"l":"test.dao"},{"l":"test.model"}];updateSearchResults();
---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dao"},{"l":"dao.adm"},{"l":"dao.book"},{"l":"dao.librarian"},{"l":"dao.loan"},{"l":"dao.reader"},{"l":"exceptions"},{"l":"model"},{"l":"test.dao"},{"l":"test.model"}];updateSearchResults();
