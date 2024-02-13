28abf461f5192bfb2af03a148f6f078493cc173f
Sara Ferreira <sara2004.souza@gmail.com>
2024-02-13 19:17:24 -0300

    💄feat: telas home do Adm e Bibliotecário

---
+++ src/main/java/org/example/controller/HomeAdmController.java
@@ -0,0 +1,120 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeAdmController {
+
+    @FXML
+    private Button buttonAllUsers;
+
+    @FXML
+    private Button buttonCount;
+
+    @FXML
+    private Button buttonGoOut;
+
+    @FXML
+    private Button buttonGoOut1;
+
+    @FXML
+    private Button buttonGoOut2;
+
+    @FXML
+    private Button buttonHome;
+
+    @FXML
+    private Button buttonLoan;
+
+    @FXML
+    private Button buttonRegisterBook;
+
+    @FXML
+    private Button buttonRegisterLoan;
+
+    @FXML
+    private Button buttonRegisterUser;
+
+    @FXML
+    private Button buttonReport;
+
+    @FXML
+    private Button buttonSearch;
+
+    @FXML
+    private Button buttonTitulo;
+
+    @FXML
+    private Button buttonTitulo1;
+
+    @FXML
+    private Button buttonTitulo11;
+
+    @FXML
+    private Button buttonTitulo12;
+
+    @FXML
+    private Button registerDevolution;
+
+    @FXML
+    void SearchBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectTitulo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openAllUsers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openLoan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRegisterBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRegisterDevolution(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRegisterLoan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openReport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerNewUser(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/org/example/controller/HomeLibrarianController.java
@@ -0,0 +1,106 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeLibrarianController {
+
+    @FXML
+    private Button buttonBooks;
+
+    @FXML
+    private Button buttonCount;
+
+    @FXML
+    private Button buttonDevolution;
+
+    @FXML
+    private Button buttonGoOut;
+
+    @FXML
+    private Button buttonHome;
+
+    @FXML
+    private Button buttonLoan;
+
+    @FXML
+    private Button buttonRegisterBook;
+
+    @FXML
+    private Button buttonRegisterLoan;
+
+    @FXML
+    private Button buttonSearch;
+
+    @FXML
+    private Button buttonTitulo;
+
+    @FXML
+    private Button buttonTitulo1;
+
+    @FXML
+    private Button buttonTitulo11;
+
+    @FXML
+    private Button buttonTitulo12;
+
+    @FXML
+    private Button registerDevolution;
+
+    @FXML
+    void SearchBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectTitulo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openBooks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openDevolution(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openLoan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRegisterBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRegisterDevolution(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openRegisterLoan(ActionEvent event) {
+
+    }
+
+}
